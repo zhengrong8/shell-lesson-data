ad8129b794d561e56cee044f02e317de23f4063f
Zhengrong Huang <zhuang8@crcfe01.crc.nd.edu>
2023-09-19 22:15:12 -0400

    s3

--- exercise-data/solution3.sh
+++ exercise-data/solution3.sh
@@ -2,7 +2,7 @@ filename="fullroster.csv"
 
 for group_id in $(seq 1 9)
 do
-	echo "Group $group_id" > rosters$group_id.csv
-	echo "Name Email_Address" >> rosters$group_id.csv
-	cat fullroster.csv | grep $group_id | cut -d ' ' -f 1,2 >> rosters$group_id.csv 
+	echo "Group $group_id" > "rosters_group$group_id.csv"
+	echo "Name Email_Address" >> "rosters_group$group_id.csv"
+	cat fullroster.csv | grep $group_id | cut -d ' ' -f 1,2 >> "rosters_group$group_id.csv" 
 done
